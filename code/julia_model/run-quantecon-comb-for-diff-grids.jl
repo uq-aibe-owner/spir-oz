6179d9870334db7548e166400a3e696d943cdc5f
Patrick OCallaghan <patrickocal@gmail.com>
2022-02-09 16:31:44 +1000

    folder and file structure

--- code/julia_model/run-quantecon-comb-for-diff-grids.jl
+++
@@ -1,9 +0,0 @@
-tOCbd = TimerOutput()
-for i in 10:10:500
-    global numLin = i
-    global grid = Vector{Float64}[]
-    for p in product(LinRange(gridMin, gridMax, numLin),LinRange(gridMin, gridMax, numLin))
-        push!(grid, collect(p))
-    end
-    evalBellCbd()
-end
