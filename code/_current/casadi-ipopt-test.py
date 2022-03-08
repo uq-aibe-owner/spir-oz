be2c9593a48c246f6ae66f2ed12e642ea31a2343
Patrick OCallaghan <patrickocal@gmail.com>
2022-03-08 20:51:36 +1000

    added parameters to nlp

--- code/_current/casadi-ipopt-test.py
+++ code/_current/casadi-ipopt-test.py
@@ -1,16 +1,34 @@
 from casadi import *
 
+p = SX.sym('p')
+
 x = SX.sym('x')
 y = SX.sym('y')
 z = SX.sym('z')
 
 nlp = nlp = {'x' : vertcat(x, y, z),
-             'f' : x ** 2 + 100 * z ** 2,
-             'g' : z + (1 - x) ** 2 - y
+             'f' : x ** p + 100 * z ** p,
+             'g' : z + (1 - x) ** p - y,
+             'p' : p
              }
 
-solver = nlpsol('solver', 'ipopt', nlp)
+opts = {"ipopt.linear_solver" : "Mumps"}
+#-----------when HSL is available, we should also be able to run:
+#opts = {"ipopt.linear_solver" : "MA27"}
+#-----------or:
+#opts = {"ipopt.linear_solver" : "MA57"}
+#-----------or:
+#opts = {"ipopt.linear_solver" : "HSL_MA86"}
+#-----------or:
+#opts = {"ipopt.linear_solver" : "HSL_MA97"}
 
-res = solver(x0=[1, 2, 3])
+#-----------the following advice comes from https://www.hsl.rl.ac.uk/ipopt/
+#-----------"when using HSL_MA86 or HSL_MA97 ensure MeTiS ordering is 
+#-----------compiled into Ipopt to maximize parallelism"
 
+solver = nlpsol('solver', 'ipopt', nlp, opts)
+
+res = solver(x0=[1, 2, 3], p = 2.5)
+print(res)
+res = solver(x0=[3, 2, 1], p = 2)
 print(res)
